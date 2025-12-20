a788a0911c5872af059412464f68fd7a63bf18cb
mdomorfaruk100 <mdomorfarukdeveloper988@gmail.com>
2025-12-20 11:15:13 +0600

    47-5-dynamic-api-and-api-parameter-and-access-params-access-query

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const users = ["Asad", 'Moni', 'Sabed', 'Susmita', 'Shohan'];
+
 app.get('/', (req, res) => {
     const fruit = {
         product: 'ada',
@@ -11,6 +13,13 @@ app.get('/', (req, res) => {
 
 app.get('/fruits/banana', (req, res) => {
     res.send({fruit:'banana',quantity: 1000 ,price: 10000})
+})
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const name = users[userId];
+    console.log(req.query);
+    res.send({id: userId, name })
 })
 
 app.listen(3000, () => console.log('Listening to port 3000'));
