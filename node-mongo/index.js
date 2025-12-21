63ba106da5737157420927b48221eef9b635689d
mdomorfaruk100 <mdomorfarukdeveloper988@gmail.com>
2025-12-21 14:55:11 +0600

    47-7-creat-POST-api-and-install-postman-read-post-data-and-use-body-parser

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
-
 app.use(cors());
+// app.use(express.json()); // This  is used to read post method json data from request body
+app.use(bodyParser.json()); // This bodyParser middleware is used to read post method json data from request body
 
 const users = ["Asad", 'Moni', 'Sabed', 'Susmita', 'Shohan'];
 
@@ -22,6 +24,13 @@ app.get('/users/:id', (req, res) => {
     const userId = req.params.id;
     const name = users[userId];
     res.send({id: userId, name })
-})
+});
+
+// post
+app.post('/addUser', (req, res)=>{
+    console.log(req); // this body object of req object is used to read post json data from post method
+    console.log(req.body); // this body object of req object is used to read post json data from post method
+    res.send(req.body)
+});
 
 app.listen(3000, () => console.log('Listening to port 3000'));
